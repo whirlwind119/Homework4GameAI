cedda6bfe65f1013fe8faf555b32821adbf0e636
whirlwind119 <tim.kim1996@gmail.com>
2017-10-14 12:28:26 -0400

    add path

---
+++ homework4/Assets/PathScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathScript : MonoBehaviour {
+
+    bool found = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () { 
+	}
+
+    public bool currentlyFalse()
+    {
+        if (found == false)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void changeStatus()
+    {
+        found = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+       if(other.gameObject.tag == "leader")
+        {
+            this.changeStatus();
+        }
+    }
+
+}
---
+++ homework4/Assets/leader.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leader : MonoBehaviour {
+
+    public float speed;
+    GameObject target;
+    GameObject holder;
+    public GameObject[] paths;
+
+
+
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        holder = findTarget();
+        if (holder != null) {
+            target = holder;
+            transform.Translate(Vector3.right * Time.deltaTime * 2);
+        }
+
+        //calculate the angle
+        if (target != null) {
+            Vector3 vectorToTarget = target.transform.position - transform.position;
+            float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+            Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * 3f);
+        }
+    }
+
+    public GameObject findTarget() {
+        int i;
+        for (i = 0; i < paths.Length; i++) {
+            if (paths[i].GetComponent<PathScript>().currentlyFalse() == true) {
+                return paths[i];
+            }
+        }
+        return null;
+    }
+}
